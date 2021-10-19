b680f9b018b62fac8bf9f9beb26759548d52150f
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-10-19 21:24:36 +0900

    カードデザインを作成中

--- lib/timeline_page.dart
+++ lib/timeline_page.dart
@@ -16,7 +16,69 @@ class FeedPage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text("FEED"),
+        child: Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            leading: Icon(Icons.album),
+            title: Text('The Enchanted Nightingale'),
+            subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network('https://picsum.photos/250?image=9'),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Text(
+                  'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+                  'Alps. Situated 1,578 meters above sea level, it is one of the '
+                  'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a ',
+                  softWrap: true,
+                  ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TextButton(
+                child: const Icon(
+                Icons.error_outline_outlined,
+              ),
+                onPressed: () { /* ... */ },
+              ),
+              TextButton(
+                child: const Icon(
+                Icons.visibility_outlined,
+              ),
+                onPressed: () { /* ... */ },
+              ),
+              TextButton(
+                child: const Icon(
+                Icons.favorite_outlined,
+              ),
+                onPressed: () { /* ... */ },
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                child: const Icon(
+                Icons.insert_comment_outlined,
+              ),
+                onPressed: () { /* ... */ },
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
+        ],
+      ),
+    ),
       ),
     );
   }
